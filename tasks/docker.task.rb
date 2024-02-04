e36d0df6b25c24ca64023b1d77d15418707700ce
Mark24 <mark.zhangyoung@gmail.com>
2024-02-04 16:45:03 +0800

    feat: docker rake

--- tasks/docker.task.rb
+++ tasks/docker.task.rb
@@ -1,17 +1,55 @@
+require_relative '../configs/setting.config.rb'
+
 namespace :docker do
+  APP_NAME = "app"
+
+  def get_label(version)
+    "#{APP_NAME}:#{version}"
+  end
+
+  def build_docker(label_name)
+    system("docker build -t #{label_name} .")
+  end
+
+  desc 'Build Docker Image'
+  task :build, [:version] do |t, args|
+    version = args[:version]
+    label_name = get_label(version)
+    puts "Your docker image version:"
+    puts label_name
+    build_docker(label_name)
+  end
+
   desc 'Push Docker'
   task :push, [:version] do |t, args|
     puts "[Message] Before run, you need login"
     version = args[:version]
+
+    label_name = get_label(version)
     puts "Your docker image version:"
-    puts version
-    # 读取version
-    image_path = "path/to/your/docker/image/repo"
-    version_label = "#{image_path}:#{version}"
-    latest_label = "#{image_path}:latest"
-    system("docker build -t #{version_label} .")
-    system("docker push #{version_label}")
-    system("docker build -t #{latest_label} .")
-    system("docker push #{latest_label}")
+    puts label_name
+    build_docker(label_name)
+    system("docker push #{label_name}")
+  end
+
+  desc 'Run Docker'
+  task :run, [:version] do |t, args|
+    puts "[Message] Before run, you need login"
+    port = App.opts[:port]
+    puts port
+    version = args[:version]
+    label_name = get_label(version)
+    cmd = <<-CMD
+    docker run  \
+    --net=host \
+    --env DATABASE_HOST=$DATABASE_HOST \
+    --env DATABASE_PORT=$DATABASE_PORT \
+    --env DATABASE_NAME=$DATABASE_NAME \
+    --env DATABASE_USER=$DATABASE_USER \
+    --env DATABASE_PASSWORD=$DATABASE_NAME \
+    -p #{port}:#{port} \
+    -it #{label_name}
+    CMD
+    system(cmd)
   end
 end
